d3059fde95b0529b2e499d714ecec9678aeec7e8
AhmedAdelFahim <ahmedadelfahim@gmail.com>
2019-02-26 21:26:28 +0200

    add video controller

---
+++ Assets/Scripts/AnimationsClips.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationsClips : MonoBehaviour
+{
+    public AnimationClip HumanIdle;
+    public AnimationClip HumanWalking;
+    public AnimationClip HumanJumping;
+    public AnimationClip HumanDancing;
+}
---
+++ Assets/Scripts/VideoController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Playables;
+
+public class NewBehaviourScript
+{
+    private PlayableGraph PlayableGraph;
+    private List<Video> Videos;
+    public void CreateVideo()
+    {
+        PlayableGraph = PlayableGraph.Create();
+    }
+
+    public void AddTrack(Video video)
+    {
+        Playable owner = Playable.Create(PlayableGraph);
+        owner.SetInputCount(1);
+        AnimationMixerPlayable mixer = AnimationMixerPlayable.Create(PlayableGraph, video.Clips.Count);
+        PlayableGraph.Connect(mixer, 0, owner, 0);
+        owner.SetInputWeight(0, 1);
+        
+        for (int clipIndex = 0 ; clipIndex < mixer.GetInputCount() ; ++clipIndex)
+        {
+            PlayableGraph.Connect(AnimationClipPlayable.Create(PlayableGraph, video.Clips[clipIndex]), 0, mixer, clipIndex);
+
+            mixer.SetInputWeight(clipIndex, 1.0f);
+
+        }
+        var playableOutput = AnimationPlayableOutput.Create(PlayableGraph, "Animation",video.animator);
+        playableOutput.SetSourcePlayable(owner);
+        playableOutput.SetSourceInputPort(0);
+        video.mixer = mixer;
+        Videos.Add(video);
+    }
+    
+    
+}
