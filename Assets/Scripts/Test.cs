bbc1b1f766e5a0427ab756e1a37c65e5c68284b0
AhmedAdelFahim <ahmedadelfahim@gmail.com>
2019-02-26 21:57:22 +0200

    add video controller

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public Animator Animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AnimationsClips animationsClips = new AnimationsClips();
+        Video video = new Video(Animator);
+        video.Clips.Add(animationsClips.HumanWalking);
+        video.Clips.Add(animationsClips.HumanWalking);
+        video.Clips.Add(animationsClips.HumanWalking);
+        video.Clips.Add(animationsClips.HumanWalking);
+        video.Clips.Add(animationsClips.HumanDancing);
+        VideoController videoController = new VideoController();
+        
+        videoController.CreateVideo();
+        
+        videoController.AddTrack();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
